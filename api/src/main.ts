b3981adb48ca5fc304e7334028b397d33dd9c455
jalipsey <jalipsey@bu.edu>
2025-09-17 21:14:51 -0500

    Adjust Ports

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,6 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
 bootstrap();
