6b198e33aa430650e780d25ba61c4e28e5f33e08
jalipsey <jalipsey@bu.edu>
2025-09-17 20:50:20 -0500

    main.ts updates

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
