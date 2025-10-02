defb5ff40caf42c602f1afd70945f2e444379664
jalipsey <jalipsey@bu.edu>
2025-10-01 19:33:41 -0500

    single lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,69 @@
-// eslint.config.js
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// eslint.config.js at repo root
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default [
-  globalIgnores(['dist']),
-  js.configs.recommended, 
-  ...tseslint.configs.recommended, 
-  reactHooks.configs['recommended-latest'],
-  reactRefresh.configs.vite,
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: tseslint.parser,
+export default {
+  root: true,
+  ignorePatterns: ['dist/', 'build/', 'node_modules/', 'eslint.config.js'],
+  overrides: [
+    // Backend (Node)
+    {
+      files: ['api/**/*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './api/tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      env: {
+        node: true,
+        jest: true,
+      },
+      plugins: ['@typescript-eslint', 'prettier'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        complexity: ['warn', 4],
+      },
     },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off', // disables the "any" rule
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      complexity: ['warn', 4]
+    // Frontend (React)
+    {
+      files: ['chit-chat-ui/**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './chit-chat-ui/tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+        jest: true,
+      },
+      plugins: ['@typescript-eslint', 'prettier', 'react-hooks', 'react-refresh'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react-refresh/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        complexity: ['warn', 4],
+      },
     },
-  }
-]
+  ],
+};
