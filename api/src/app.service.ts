25f0c463c747fc85a3058d9efcc84cee3ee9f73f
Ardit <abriskaj@bu.edu>
2025-10-09 17:17:02 -0400

    fixed groq

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,9 +3,9 @@ import { basePrompt } from './ai.prompt';
 import Groq from 'groq-sdk';
 import 'dotenv/config';
 
-// const groq = new Groq({
-//   apiKey: process.env.GROQ_API_KEY,
-// });
+const groq = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
 
 @Injectable()
 export class AppService {
@@ -13,21 +13,21 @@ export class AppService {
     return 'Hello World!';
   }
 
-  // async getLlmAnswer(userMessage: string): Promise<string> {
-  //   try {
-  //     const completion = await groq.chat.completions.create({
-  //       model: 'groq/compound-mini',
-  //       messages: [
-  //         { role: 'system', content: basePrompt },
-  //         { role: 'user', content: userMessage },
-  //       ],
-  //     });
-  //     return (
-  //       completion.choices?.[0]?.message?.content ?? 'No response from AI.'
-  //     );
-  //   } catch (error) {
-  //     console.error('Groq API error:', error);
-  //     return 'Sorry, there was an error contacting the AI service.';
-  //   }
-  // }
+  async getLlmAnswer(userMessage: string): Promise<string> {
+    try {
+      const completion = await groq.chat.completions.create({
+        model: 'groq/compound-mini',
+        messages: [
+          { role: 'system', content: basePrompt },
+          { role: 'user', content: userMessage },
+        ],
+      });
+      return (
+        completion.choices?.[0]?.message?.content ?? 'No response from AI.'
+      );
+    } catch (error) {
+      console.error('Groq API error:', error);
+      return 'Sorry, there was an error contacting the AI service.';
+    }
+  }
 }
