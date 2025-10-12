055ff63e13340bd6b0f41529c34670a679f1a79b
jalipsey <jalipsey@bu.edu>
2025-10-12 08:15:22 -0500

    Use environment variable for host

--- api/src/main.ts
+++ api/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     origin: 'http://localhost:8000',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? '0.0.0.0');
 }
 bootstrap();
